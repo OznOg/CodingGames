2dfc3fec3ce4a5fed124408639a228a3346f1cab
Sébastien Gonzalve <sebastien.gonzalve@aliceadsl.fr>
2021-10-21 18:47:12 +0200

    gna

--- sample.cpp
+++ sample.cpp
@@ -8,19 +8,20 @@ using namespace std;
 
 std::string doit(int size, std::vector<unsigned int> a) {
 
-  std::vector<unsigned int> b(size / 16); // <- output tab
  
+ uint64_t B = 0;
+
  uint64_t A = a[1];
  A <<= 32;
  A += a[0];
+
   for (int i = 0; i < size; i++) {
       auto bi = (A >> (i % 32)) & 1;
 
       for (int j = 0; j < size; j++) {
           auto bj = (A >> ((j % 32) + 32)) & 1;
 
-          std::div_t dij = std::div(i + j, 32);
-          b[dij.quot] ^= ( bi & bj) << dij.rem;   // Magic centaurian operation
+          B ^= ( bi & bj) << i + j;   // Magic centaurian operation
       }
   }
 
@@ -31,6 +32,9 @@ std::string doit(int size, std::vector<unsigned int> a) {
 //  000XXXXXX
 
 
+  std::vector<unsigned int> b(2);
+  b[0] = B & 0xFFFFFFFF;
+  b[1] = (B>>32) & 0xFFFFFFFF;
   std::stringstream out;
   for(int i = 0; i < size / 16; i++) {
     if (i > 0) {
